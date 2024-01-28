cdaaf7069621a145cbd54e5da2f4b3b496b2258a
Furkan <40832418+Benfrknd@users.noreply.github.com>
2024-01-28 18:15:48 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#0F0F0F',
+        'second': '#005B41',
+        'midnight': '#040D12',
+        'metal': '#183D3D',
+        'tahiti': '#3F2E3E',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#A5C9CA',
+      },
+    },
+  },
+  plugins: [],
+}
+
